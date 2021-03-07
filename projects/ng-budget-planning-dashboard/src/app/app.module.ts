f5cec035ae130a55b3f9e852f95a57c28eb30f70
João Victor <joaovictor_jorge@outlook.com>
2021-03-07 10:28:33 -0300

    remove ng ant design of budget planning

--- projects/ng-budget-planning-dashboard/src/app/app.module.ts
+++ projects/ng-budget-planning-dashboard/src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { pt_BR } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import pt from '@angular/common/locales/pt';
 import { FormsModule } from '@angular/forms';
@@ -18,7 +16,6 @@ registerLocaleData(pt);
 @NgModule({
     declarations: [AppComponent, CommonLayout],
     imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule, SharedModule],
-    providers: [{ provide: NZ_I18N, useValue: pt_BR }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
